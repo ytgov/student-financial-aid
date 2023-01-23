b017ea74581e28703fa7e573fb768ecb6c3e528a
Michael Johnson <michael@icefoganalytics.com>
2023-01-23 15:28:38 -0700

    Basic Node/Vue3 Setup

---
+++ src/web/src/modules/home/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <data-entry></data-entry>
+</template>
+
+<script lang="ts">
+import DataEntry from "@/modules/centre/components/DataEntry.vue";
+export default {
+  name: "Dashboard",
+  components: { DataEntry },
+};
+</script>
