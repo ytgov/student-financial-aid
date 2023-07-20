3b6d6a4123467d1a77d9aef7dde818c4de9c0be2
Michael Johnson <michael@icefoganalytics.com>
2023-07-20 10:18:00 -0700

    Format changes

--- src/web/src/components/terms/atipp.vue
+++ src/web/src/components/terms/atipp.vue
@@ -1,62 +1,116 @@
 <template>
   <div>
     <h1>TERMS OF USE AND RELEASE OF LIABILITY</h1>
-    <p>Please read the following Terms of Service ("Agreement") carefully. In consideration for accessing and using this website and all of its contents, information, and related links to websites (collectively, the "Website"), you and your successors understand, acknowledge, and agree to the following terms:</p>
+    <p>
+      Please read the following Terms of Service ("Agreement") carefully. In consideration for accessing and using this
+      website and all of its contents, information, and related links to websites (collectively, the "Website"), you and
+      your successors understand, acknowledge, and agree to the following terms:
+    </p>
 
     <h2 id="WARRANTY_DISCLAIMER">WARRANTY DISCLAIMER</h2>
     <p>
-      1. Use of this Website is SOLELY AT YOUR OWN RISK, and you freely and voluntarily, accept all risks and dangers related to access and use of the Website. This Website is provided "AS IS". Government of Yukon ("YG") makes NO GUARANTEE, REPRESENTATION, OR WARRANTY OF ANY KIND OR IN ANY WAY, EXPRESS OR IMPLIED, WITH RESPECT TO: THE CONTENT, VERACITY, ACCURACY, AVAILABILITY, SUITABILITY, RELIABILITY, USABILITY, FUNCTIONALITY, COMPLETENESS, OR TIMELINESS OF THE WEBSITE; merchantability, or fitness for a particular purpose; non-infringement of rights; safety; quality; errors or omissions on the Website; or that the use of the Website will be uninterrupted, virus-free or error-free. You assume full and sole responsibility for verifying that any information or contents provided on or through the Website is: accurate, current and complete.
+      1. Use of this Website is SOLELY AT YOUR OWN RISK, and you freely and voluntarily, accept all risks and dangers
+      related to access and use of the Website. This Website is provided "AS IS". Government of Yukon ("YG") makes NO
+      GUARANTEE, REPRESENTATION, OR WARRANTY OF ANY KIND OR IN ANY WAY, EXPRESS OR IMPLIED, WITH RESPECT TO: THE
+      CONTENT, VERACITY, ACCURACY, AVAILABILITY, SUITABILITY, RELIABILITY, USABILITY, FUNCTIONALITY, COMPLETENESS, OR
+      TIMELINESS OF THE WEBSITE; merchantability, or fitness for a particular purpose; non-infringement of rights;
+      safety; quality; errors or omissions on the Website; or that the use of the Website will be uninterrupted,
+      virus-free or error-free. You assume full and sole responsibility for verifying that any information or contents
+      provided on or through the Website is: accurate, current and complete.
     </p>
 
     <h2 id="RELEASE_OF_LIABILITY">RELEASE OF LIABILITY</h2>
     <p>
-      2. In consideration for access to and use of the Website, you fully and irrevocably agree to FULLY RELEASE, REMISE, WAIVE and FOREVER DISCHARGE YG, its elected officials, officers, employees, partners, agents, insurers, successors and assigns from any and all claims, liabilities, actions, proceedings, damages (including: direct, indirect, consequential, incidental, general, special or exemplary damages, any economic losses, lost profits, lost opportunities, or all other costs, expenses and losses of any type or for any reason), or any other legal remedy in any way or for any reason, WHETHER UNKNOWN OR UNFORESEEN, whether in law or in equity or pursuant to statute or regulation (collectively, the "Claims") arising from, related to, or in any way connected to your access to or use of the Website, including, without limitation: NEGLIGENCE; GROSS NEGLIGENCE; MISREPRESENTATION (NEGLIGENT OR OTHERWISE); BREACH OF ANY STATUTORY DUTY OR OTHER DUTY, ANY INJURY OR DEATH TO YOU; DAMAGE OR LOSS to PROPERTY; the content, veracity, accuracy, availability, suitability, reliability, usability, functionality, compliance, completeness or timeliness of the Website; any failure to receive, transmit, download, or use any materials, documents, and information related to or arising from the Website; any change to, suspension of, or termination of access to or use of the Website; or any claim in tort; bad faith or violation of common law duties; breach of contract; or any decisions, acts, omissions or errors by YG, its elected officials, officers, employees, partners, agents, insurers, successors and assigns related to or arising from the Website or your access to and use of the Website in any way.
+      2. In consideration for access to and use of the Website, you fully and irrevocably agree to FULLY RELEASE,
+      REMISE, WAIVE and FOREVER DISCHARGE YG, its elected officials, officers, employees, partners, agents, insurers,
+      successors and assigns from any and all claims, liabilities, actions, proceedings, damages (including: direct,
+      indirect, consequential, incidental, general, special or exemplary damages, any economic losses, lost profits,
+      lost opportunities, or all other costs, expenses and losses of any type or for any reason), or any other legal
+      remedy in any way or for any reason, WHETHER UNKNOWN OR UNFORESEEN, whether in law or in equity or pursuant to
+      statute or regulation (collectively, the "Claims") arising from, related to, or in any way connected to your
+      access to or use of the Website, including, without limitation: NEGLIGENCE; GROSS NEGLIGENCE; MISREPRESENTATION
+      (NEGLIGENT OR OTHERWISE); BREACH OF ANY STATUTORY DUTY OR OTHER DUTY, ANY INJURY OR DEATH TO YOU; DAMAGE OR LOSS
+      to PROPERTY; the content, veracity, accuracy, availability, suitability, reliability, usability, functionality,
+      compliance, completeness or timeliness of the Website; any failure to receive, transmit, download, or use any
+      materials, documents, and information related to or arising from the Website; any change to, suspension of, or
+      termination of access to or use of the Website; or any claim in tort; bad faith or violation of common law duties;
+      breach of contract; or any decisions, acts, omissions or errors by YG, its elected officials, officers, employees,
+      partners, agents, insurers, successors and assigns related to or arising from the Website or your access to and
+      use of the Website in any way.
     </p>
 
     <h2 id="INTERUPTION_AND_USE">INTERUPTION AND USE</h2>
     <p>
-      3. YG is not responsible or liable in any way or for any reason for failure to receive or misdirected notices or communications from or related to the Website, which may result in the failure to update information.
+      3. YG is not responsible or liable in any way or for any reason for failure to receive or misdirected notices or
+      communications from or related to the Website, which may result in the failure to update information.
     </p>
 
     <p>
-      4. You acknowledge and agree that the Website may be interrupted or unavailable at times due to technical issues, maintenance, or for other reasons.
+      4. You acknowledge and agree that the Website may be interrupted or unavailable at times due to technical issues,
+      maintenance, or for other reasons.
     </p>
 
     <p>
-      5. YG may, in its sole and absolute discretion, change, suspend, or terminate access to or use of the Website at any time without prior notice to you. In the case of a computer virus or other malicious electronic code that, in the sole opinion of YG is considered harmful to YG's computer networks, YG reserves the right to take whatever actions it deems necessary to rectify, mitigate, or neutralize the computer virus or other malicious electronic code, including, without limitation: discontinuing access to or use of the Website at any time or deleting or modifying any electronic transmission or communication to you or by you.
+      5. YG may, in its sole and absolute discretion, change, suspend, or terminate access to or use of the Website at
+      any time without prior notice to you. In the case of a computer virus or other malicious electronic code that, in
+      the sole opinion of YG is considered harmful to YG's computer networks, YG reserves the right to take whatever
+      actions it deems necessary to rectify, mitigate, or neutralize the computer virus or other malicious electronic
+      code, including, without limitation: discontinuing access to or use of the Website at any time or deleting or
+      modifying any electronic transmission or communication to you or by you.
     </p>
 
     <h2 id="GENERAL">GENERAL</h2>
     <p>
-      6. YG may, at its sole discretion, change the terms and conditions of this Agreement from time to time without notice to you. It is your responsibility to review this Agreement to satisfy yourself that you continue to agree to the terms and conditions set out in this Agreement or any amendment thereof.
+      6. YG may, at its sole discretion, change the terms and conditions of this Agreement from time to time without
+      notice to you. It is your responsibility to review this Agreement to satisfy yourself that you continue to agree
+      to the terms and conditions set out in this Agreement or any amendment thereof.
     </p>
 
     <p>
-      7. This Agreement will be deemed to have been made in and will be governed, interpreted and enforced by the laws of the Yukon without regard to conflict of law principles that would impose a law of another jurisdiction, and you irrevocably attorn and submit to the exclusive jurisdiction of the courts in Yukon.
+      7. This Agreement will be deemed to have been made in and will be governed, interpreted and enforced by the laws
+      of the Yukon without regard to conflict of law principles that would impose a law of another jurisdiction, and you
+      irrevocably attorn and submit to the exclusive jurisdiction of the courts in Yukon.
     </p>
 
     <p>
-      8. Each term of this Agreement will be valid and enforceable to the fullest extent permitted by law. If any provision of this Agreement is held to be invalid, unenforceable or illegal to any extent, such provision may be severed and such invalidity, unenforceability or illegality will not prejudice or affect the validity, enforceability and legality of the remaining provisions of this Agreement. If any such term of this Agreement is held to be invalid, unenforceable or illegal, the parties will promptly endeavor in good faith to negotiate new terms to eliminate such invalidity, unenforceability, or illegality and to restore this Agreement as nearly as possible to its original intent and effect.
+      8. Each term of this Agreement will be valid and enforceable to the fullest extent permitted by law. If any
+      provision of this Agreement is held to be invalid, unenforceable or illegal to any extent, such provision may be
+      severed and such invalidity, unenforceability or illegality will not prejudice or affect the validity,
+      enforceability and legality of the remaining provisions of this Agreement. If any such term of this Agreement is
+      held to be invalid, unenforceable or illegal, the parties will promptly endeavor in good faith to negotiate new
+      terms to eliminate such invalidity, unenforceability, or illegality and to restore this Agreement as nearly as
+      possible to its original intent and effect.
     </p>
 
     <p>
-      9. No action or failure to act by YG shall constitute a waiver of any right or duty afforded to YG under the Agreement, nor shall any such action or failure to act constitute an approval of or acquiescence of any breach thereunder, except as may be specifically and explicitly agreed to in writing by YG.
+      9. No action or failure to act by YG shall constitute a waiver of any right or duty afforded to YG under the
+      Agreement, nor shall any such action or failure to act constitute an approval of or acquiescence of any breach
+      thereunder, except as may be specifically and explicitly agreed to in writing by YG.
     </p>
 
     <p>
-      10. You shall execute such further documents and give such further assurances as are reasonably required to give effect to this Agreement.
+      10. You shall execute such further documents and give such further assurances as are reasonably required to give
+      effect to this Agreement.
     </p>
 
     <p>
-      11. Nothing in this Agreement shall create the relationship of principal and agent, employer and employee, partnership, or joint venture between you and YG.
+      11. Nothing in this Agreement shall create the relationship of principal and agent, employer and employee,
+      partnership, or joint venture between you and YG.
     </p>
 
     <p>
-      12. The terms and conditions set out in this Agreement are the entire agreement between you and YG relating to your access and use of the Website, and supersedes all previous communications, representations, negotiations, understandings and other agreements, whether oral or written, between the parties. Where a conflict exists between this Agreement and any other terms of service related to or arising from the Website, these terms of service shall govern.
+      12. The terms and conditions set out in this Agreement are the entire agreement between you and YG relating to
+      your access and use of the Website, and supersedes all previous communications, representations, negotiations,
+      understandings and other agreements, whether oral or written, between the parties. Where a conflict exists between
+      this Agreement and any other terms of service related to or arising from the Website, these terms of service shall
+      govern.
     </p>
 
-    <p>
-      13. You have read this Agreement thoroughly and fully understand its terms, and acknowledge and agree that by clicking the "YES" button you are fully accepting and agreeing to the terms of this Agreement, and this will be deemed equivalent to affixing an electronic signature for the purposes of this Agreement and the Electronic Commerce Act, RSY 2002, c. 66.
+    <p style="margin-bottom: 0px">
+      13. You have read this Agreement thoroughly and fully understand its terms, and acknowledge and agree that by
+      clicking the "YES" button you are fully accepting and agreeing to the terms of this Agreement, and this will be
+      deemed equivalent to affixing an electronic signature for the purposes of this Agreement and the Electronic
+      Commerce Act, RSY 2002, c. 66.
     </p>
   </div>
 </template>
@@ -64,3 +118,16 @@
 <script>
 export default {};
 </script>
+
+<style scoped>
+h1 {
+  margin-top: 0px;
+}
+p {
+  margin-bottom: 10px;
+}
+h2 {
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+</style>
