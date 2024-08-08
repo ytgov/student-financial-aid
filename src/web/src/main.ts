48cbde6f11bf418b59b6b7ccc9971da74c28bb14
Michael Johnson <michael@icefoganalytics.com>
2024-08-08 09:53:27 -0700

    Enabling analytics through Matomo

--- src/web/src/main.ts
+++ src/web/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import i18n from "./i18n";
 import { router } from "./routes";
+import VueMatomo from "vue-matomo";
 
 // Plugins
 import { registerPlugins } from "./plugins";
@@ -11,6 +12,14 @@ const pinia = createPinia();
 
 const app = createApp(App);
 app.use(pinia).use(router).use(i18n);
+
+app.use(VueMatomo, {
+  // Configure your matomo server and site by providing
+  host: "https://analytics.gov.yk.ca",
+  siteId: 82,
+  router: router,
+});
+
 registerPlugins(app);
 
 app.mount("#app");
